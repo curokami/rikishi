46c3a2b1432cbbcfde77bc40e30738753e9b9e1b
curokami <berettaxx@gmail.com>
2024-08-19 11:36:33 +0900

    update

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ print(df.head())
 
 
 print('最も多くの力士を輩出した出身地はどこですか？')
+
+#力士の出身地カラムを抽出。
 df_Shusshin=df[["Shusshin"]] 
 
 #print(df_Shusshin.head())
 
-#df_Shusshin.value_counts()
-
+# カラムの値をカウントし、辞書型に変換する。
 d = df_Shusshin.value_counts().to_dict()  
 keys = [k for k, v in d.items() if v == 1449]
 
@@ -19,6 +20,23 @@ aomori_list = [item[0] for item in keys]
 
 print(f"力士輩出、最多出身地は{aomori_list[0]}です。")
 
+
+print('片男波で練習している力士は誰ですか?')
+
+#まず、データフレームから部屋名カラムからkataonamiを抽出したデータフレームを作成する。
+df_Kataonami= df[df['Heya'] == 'Kataonami']
+
+#df_Kataonamiからに所属する力士の名前を取り出す。
+
+df_meuber_kataonami = df_Kataonami['Rikishi'].unique()
+print(df_meuber_kataonami.shape)
+
+rikishi_count = 0
+for i in df_meuber_kataonami:
+    rikishi_count += 1
+    print(i)
+    
+print(f"片男波所属の力士は上記の{rikishi_count}にんです。")    
 
 
 
